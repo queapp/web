3323135d3f8b906bdaea94916f902f0679519b36
1egoman <rsg1egoman@gmail.com>
2015-01-01 08:38:47 -0500

    working online documentation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var serveStatic = require('serve-static');
 var path = require('path');
+var request = require("request");
+var marked = require("marked");
 
 // port to serve on
 port = process.env.PORT || process.argv[2] || 8000;
@@ -19,13 +21,19 @@ app.get("/", function(req, res) {
   });
 });
 
-app.get("/docs", function(req, res) {
-  res.render("docs", {
-    heroTitle: "Que's Documentation",
-    byline: false,
-    page: "Docs"
+app.get("/docs/:page", function(req, res) {
+  // get the documentation
+  filename = req.param('path') || "index";
+  request.get("https://raw.githubusercontent.com/queapp/core/master/docs/" + filename + ".md", function(status, resp, body) {
+    res.render("docs", {
+      heroTitle: "Que's Documentation",
+      byline: false,
+      page: "Docs",
+      content: marked(body)
+    });
   });
 });
+app.get("/docs", function(req, res) { res.redirect("/docs/index") });
 
 app.listen(port, function(server) {
   console.log("Hosted on :"+port);
