16c58a1af72383d7abe488a3fc9940c42e70bbfc
1egoman <rsg1egoman@gmail.com>
2015-01-01 08:21:03 -0500

    new website format: ejs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var connect = require('connect');
+var express = require('express');
 var serveStatic = require('serve-static');
 var path = require('path');
 
@@ -6,8 +6,27 @@ var path = require('path');
 port = process.env.PORT || process.argv[2] || 8000;
 
 // serve the site
-connect()
-  .use(serveStatic(path.join(__dirname, 'public')))
-  .listen(port, function(server) {
-    console.log("Hosted on :"+port);
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(serveStatic(path.join(__dirname, 'views')))
+
+app.get("/", function(req, res) {
+  res.render("index", {
+    heroTitle: "Introducing Que",
+    byline: true,
+    page: "Home"
   });
+});
+
+app.get("/docs", function(req, res) {
+  res.render("docs", {
+    heroTitle: "Que's Documentation",
+    byline: false,
+    page: "Docs"
+  });
+});
+
+app.listen(port, function(server) {
+  console.log("Hosted on :"+port);
+});
